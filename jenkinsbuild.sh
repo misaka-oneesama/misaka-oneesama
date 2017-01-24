49fd0ddfde35960c6ef3f6b0452c3d6c60dc7fb1
GhettoGirl <magiruuvelvet@naver.com>
2017-01-25 00:53:17 +0100

    QDiscord: use my version of QDiscord for more flexibility

--- jenkinsbuild.sh
+++ jenkinsbuild.sh
@@ -4,9 +4,12 @@
 cd "$(dirname "$(realpath "$0")")"
 #########################################################################
 
+# note to self: GCC compiled binary segfaults on FreeBSD
+
 ### Setup environment
 
-export QMAKESPEC=/usr/local/lib/qt5/mkspecs/freebsd-clang
+export QMAKE_BIN="/usr/local/lib/qt5/bin/qmake"
+export QMAKESPEC="/usr/local/lib/qt5/mkspecs/freebsd-clang"
 
 # Make sure that there are no old builds
 [ -d build ] && rm -rf build
@@ -20,5 +23,5 @@ mkdir build && cd build
 #########################################################################
 
 # Build
-qmake ../misaka-oneesama.pro
+"$QMAKE_BIN" ../misaka-oneesama.pro
 make -j4
